f7d2a3c4f056590ac86e053c4179607fdf57ae84
AnzorKarto <beit06@mail.ru>
2022-11-07 13:40:43 +0300

    Сделал первую таску

---
+++ first.js
@@ -0,0 +1,7 @@
+let one = 1
+let two = 2
+
+function sum() {
+  return one + two
+}
+export default { one: one, two: two, sum: sum }
---
+++ second.js
@@ -0,0 +1,4 @@
+import obj from './first.js'
+console.log(obj)
+console.log(obj.one)
+console.log(obj.sum())
